e132952043e05933d4f3d0d6944c832f6fb7fd86
My java <ankit.sharma21598@gmail.com>
2023-07-29 23:19:18 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Sharma-Tech</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/ankit-sharma-500a43a8/" target={"blank"}>
+            LinkedIN
+          </a>
+          <a href="https://www.instagram.com/ankit.sharma21598/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/ankitsharma21598" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
